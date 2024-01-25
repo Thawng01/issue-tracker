a5c9eb095e4e783638e0453fa99fe76b13f36977
thechin press <thechinpress@gmail.com>
2024-01-25 19:33:44 +0630

    Initial and Building Navbar

---
+++ app/NavBar.tsx
@@ -0,0 +1,38 @@
+"use client";
+import Link from "next/link";
+import React from "react";
+import classNames from "classnames";
+import { usePathname } from "next/navigation";
+import { BsBugFill } from "react-icons/bs";
+
+const links = [
+    { label: "Dashboard", href: "/" },
+    { label: "Issues", href: "/issues" },
+];
+const NavBar = () => {
+    const currentPath = usePathname();
+    return (
+        <nav className="flex space-x-6 px-6 h-14 items-center border-b">
+            <Link href="/">
+                <BsBugFill className="text-2xl" />
+            </Link>
+            <ul className="flex space-x-6">
+                {links.map((link) => (
+                    <Link
+                        key={link.href}
+                        href={link.href}
+                        className={classNames({
+                            "text-zinc-900": currentPath === link.href,
+                            "text-zinc-500": currentPath !== link.href,
+                            "hover:text-zinc-800 transition-colors": true,
+                        })}
+                    >
+                        {link.label}
+                    </Link>
+                ))}
+            </ul>
+        </nav>
+    );
+};
+
+export default NavBar;
