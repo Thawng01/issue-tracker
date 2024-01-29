2b0cd57b84e0e0873755a33f318abf460e0b645d
thechin press <thechinpress@gmail.com>
2024-01-29 12:33:26 +0630

    Add login and logout button

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -4,6 +4,8 @@ import React from "react";
 import classNames from "classnames";
 import { usePathname } from "next/navigation";
 import { BsBugFill } from "react-icons/bs";
+import { useSession } from "next-auth/react";
+import { Box } from "@radix-ui/themes";
 
 const links = [
     { label: "Dashboard", href: "/" },
@@ -11,6 +13,8 @@ const links = [
 ];
 const NavBar = () => {
     const currentPath = usePathname();
+    const { status, data: session } = useSession();
+
     return (
         <nav className="flex space-x-6 px-6 h-14 items-center border-b">
             <Link href="/">
@@ -18,19 +22,29 @@ const NavBar = () => {
             </Link>
             <ul className="flex space-x-6">
                 {links.map((link) => (
-                    <Link
-                        key={link.href}
-                        href={link.href}
-                        className={classNames({
-                            "text-zinc-900": currentPath === link.href,
-                            "text-zinc-500": currentPath !== link.href,
-                            "hover:text-zinc-800 transition-colors": true,
-                        })}
-                    >
-                        {link.label}
-                    </Link>
+                    <li key={link.href}>
+                        <Link
+                            key={link.href}
+                            href={link.href}
+                            className={classNames({
+                                "text-zinc-900": currentPath === link.href,
+                                "text-zinc-500": currentPath !== link.href,
+                                "hover:text-zinc-800 transition-colors": true,
+                            })}
+                        >
+                            {link.label}
+                        </Link>
+                    </li>
                 ))}
             </ul>
+            <Box>
+                {status === "authenticated" && (
+                    <Link href="/api/auth/signout">Logout</Link>
+                )}
+                {status === "unauthenticated" && (
+                    <Link href="/api/auth/signin">Login</Link>
+                )}
+            </Box>
         </nav>
     );
 };
