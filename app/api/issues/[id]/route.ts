5547e921ec73473cc4a307aa884ade14adfbca0d
thechin press <thechinpress@gmail.com>
2024-01-29 23:03:58 +0630

    add assigned user to issue

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,22 +1,29 @@
-import { IssueSchema } from "@/app/validationSchema";
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/prisma/client";
 import { getServerSession } from "next-auth";
+
 import { authOptions } from "@/app/auth/authOptions";
+import { patchIssueSchema } from "@/app/validationSchema";
 
 
 export async function PATCH(request: NextRequest,
     { params }: { params: { id: string } }
 ) {
 
-    const session = await getServerSession(authOptions)
-    if (!session) return NextResponse.json("Forbidden", { status: 401 })
+    // const session = await getServerSession(authOptions)
+    // if (!session) return NextResponse.json("Forbidden", { status: 401 })
 
     const body = await request.json()
-    const validation = IssueSchema.safeParse(body)
+    const validation = patchIssueSchema.safeParse(body)
 
     if (!validation.success) {
         return NextResponse.json(validation.error.format(), { status: 400 })
+    }
+
+    const { assignedUserId, title, description } = body
+    if (assignedUserId) {
+        const user = await prisma.user.findUnique({ where: { id: assignedUserId } })
+        if (!user) return NextResponse.json({ error: "Invalid Id. " }, { status: 400 })
     }
 
     const issue = await prisma.issue.findUnique({
@@ -24,14 +31,15 @@ export async function PATCH(request: NextRequest,
     })
 
     if (!issue) {
-        return NextResponse.json("No issue found with the given Id.", { status: 404 })
+        return NextResponse.json({ error: "No issue found with the given Id." }, { status: 404 })
     }
 
     const updatedIssue = await prisma.issue.update({
         where: { id: params.id },
         data: {
-            title: body.title,
-            description: body.description
+            title,
+            description,
+            assignedUserId
         }
     })
 
--- app/validationSchema.ts
+++ app/validationSchema.ts
@@ -4,3 +4,9 @@ export const IssueSchema = z.object({
     title: z.string().min(1, "Title is required."),
     description: z.string().min(1, "Description is required.")
 });
+
+export const patchIssueSchema = z.object({
+    title: z.string().min(1, "Title is required.").optional(),
+    description: z.string().min(1, "Description is required.").optional(),
+    assignedUserId: z.string().min(1, "AssignedUserId is required.").optional()
+});
